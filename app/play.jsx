dbccde9c8a3ca82c480a038bbf14e18c62c84530
ongzhili <ongchili234@gmail.com>
2023-05-25 23:57:47 +0800

    add placeholder pages
    title

--- app/play.jsx
+++ app/play.jsx
@@ -1,13 +1,80 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { View } from 'react-native';
-import { Text } from 'react-native-paper';
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, FlatList, Image } from 'react-native';
+import { Text, Button } from 'react-native-paper';
+import { Link } from 'expo-router';
 
 
-export default function commHome() {
+
+const DATA = [
+    {
+      id: '1',
+      title: 'Decks',
+      link: "deckPlay",
+    },
+    {
+      id: '2',
+      title: 'Create Decks',
+      link: "deckBuild",
+    },
+  ];
+
+function Item( {item} ) { 
     return (
-        <SafeAreaView>
-            <Text>this is the Play
-            </Text>
-        </SafeAreaView>
+        <View style = {styles.button}>
+          <Link href= {item.link}>
+            <Button style = {styles.button}>
+                <Text style = {styles.title}>
+                    {item.title}
+                </Text>
+            </Button>
+          </Link>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    button: {
+      backgroundColor: '#D9D9D9',
+      padding: 10,
+      marginVertical: 8,
+      marginHorizontal: 40,
+    },
+    title: {
+      textAlign: 'center',
+      fontSize: 25,
+      alignSelf: 'stretch',
+      lineHeight: 30,
+    },
+    image: {
+      flex: 1,
+      width: 400,
+      height: 400,
+      resizeMode: 'stretch' ,
+      padding: 100,
+      marginVertical:20,
+    }
+  });
+
+
+
+export default function Play() {
+    return (
+      <View>
+        <Image style = {styles.image} source = {require('../assets/adaptive-icon.png')}>
+        </Image>
+        <FlatList
+        data = {DATA}
+        renderItem = {({item}) => <Item item={item} />}
+        keyExtractor={item => item.id}>
+
+        </FlatList>
+
+      </View>
     );
 }
