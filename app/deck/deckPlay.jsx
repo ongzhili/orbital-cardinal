499a9323c776df2a83cc767a748145361aa5aac6
Wyrkx <wyrkx@DEUS-MACHINA>
2023-06-14 00:41:43 +0800

    Implemented Watermelondb and Deckplay with flipping card

--- app/deck/deckPlay.jsx
+++ app/deck/deckPlay.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { View } from 'react-native';
+import { View, ScrollView, Dimensions } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, FlatList, Image } from 'react-native';
 import { Text, Button, TextInput } from 'react-native-paper';
 import { Link } from 'expo-router';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FlashCard from './FlashCard';
 import { DeckContext } from '../../contexts/deck';
+import FlipCard from 'react-native-flip-card';
+import Swiper from 'react-native-deck-swiper';
+import { Card, Deck } from '../../lib/model'
 
 
 const SAMPLE_FLASHCARDS = [
@@ -23,21 +26,75 @@ const SAMPLE_FLASHCARDS = [
   },
 ]
 
-export default function deckPlay() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
-  const currentDeck = useContext(DeckContext);
-    return (
-      <SafeAreaView>
-      <FlatList
-      data = {currentDeck.deck}
-      renderItem = {({item}) => <FlashCard flashCard={item} />}
-      keyExtractor={item => item.id}>
 
-      </FlatList>
-      </SafeAreaView>
-
-    );
+/**
+ * @param {Card} card 
+ * @returns html
+ */
+function renderCard(cards) {
+  console.log(cards)
+  card = cards
+  return (
+    <ScrollView>
+      <FlipCard
+        style = { styles.card }
+      >
+        {/* Front */}
+        <View style = {styles.front}>
+          <Text style = {styles.title}> {card.front} </Text>
+        </View>
+        {/* Back */}
+        <View style = {styles.back}>
+          <Text style = {styles.title}> {card.back} </Text>
+        </View>
+      </FlipCard>
+    </ScrollView>
+  )
 }
+
+const ini = Card.makeCard("Name","front", "back")
+
+export default function deckPlay() {
+  const [card, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  ini.then(card => setCards(card)).then(x => setLoading(false))
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlipCard
+          style = { styles.card }
+        >
+          {/* Front */}
+          <View style = {styles.front}>
+            <Text style = {styles.title}> {card.front} </Text>
+          </View>
+          {/* Back */}
+          <View style = {styles.back}>
+            <Text style = {styles.title}> {card.back} </Text>
+          </View>
+        </FlipCard>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+// export default function deckPlay() {
+//   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+//   const currentDeck = useContext(DeckContext);
+//     return (
+//       <SafeAreaView>
+//       <FlatList
+//       data = {currentDeck.deck}
+//       renderItem = {({item}) => <FlashCard flashCard={item} />}
+//       keyExtractor={item => item.id}>
+
+//       </FlatList>
+//       </SafeAreaView>
+
+//     );
+// }
+
+const { width, height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
@@ -61,5 +118,28 @@ const styles = StyleSheet.create({
   input: {
     marginBottom: 10,
   },
+  card: {
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    shadowOpacity: 0.5,
+    shadowOffset: {
+      height: 5,
+      width: 5,
+    },
+    width: width - 20,
+    height: height - 200,
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 0,
+  },
+  image: {
+    flex: 1,
+    width: 400,
+    height: 400,
+    resizeMode: 'stretch' ,
+    padding: 100,
+    marginVertical:20,
+  }
   });
 
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 import "expo-router/entry";
+import 'react-native-gesture-handler';
+
+// WatermelonDB setup
+import { Database } from '@nozbe/watermelondb'
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
+
+import schema from './lib/model/schema'
+import migrations from './lib/model/migrations'
+// import Post from './model/Post' // ⬅️ You'll import your Models here
+import { Card, CardDeck, CardTag, Deck, Tag } from "./lib/model"
+
+// First, create the adapter to the underlying database:
+const adapter = new SQLiteAdapter({
+  schema,
+  // (You might want to comment it out for development purposes -- see Migrations documentation)
+  migrations,
+  // (optional database name or file system path)
+  dbName: 'cardinal',
+  // (recommended option, should work flawlessly out of the box on iOS. On Android,
+  // additional installation steps have to be taken - disable if you run into issues...)
+  jsi: true, /* Platform.OS === 'ios' */
+  // (optional, but you should implement this method)
+  onSetUpError: error => {
+    // Database failed to load -- offer the user to reload the app or log out
+    console.log('asd')
+  }
+})
+
+// Then, make a Watermelon database from it!
+export const database = new Database({
+  adapter,
+  modelClasses: [
+    Card, CardDeck, CardTag, Deck, Tag
+  ],
+})
