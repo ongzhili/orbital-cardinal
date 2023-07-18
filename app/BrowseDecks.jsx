d14e1c51f8fe59b45cf7eb246ac0271b30ae31ab
Wyrkx <107403096+Wyrkx@users.noreply.github.com>
2023-07-19 02:48:30 +0800

    BrowseDecks update

--- app/BrowseDecks.jsx
+++ app/BrowseDecks.jsx
@@ -3,138 +3,142 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ChoiceRender } from "./community/browseGuilds";
 import { View, FlatList, StyleSheet, Text } from "react-native";
 import { supabase } from "../lib/supabase";
-import { Button } from "react-native-paper";
+import { Button, Card } from "react-native-paper";
+import { synchronize } from '@nozbe/watermelondb/sync'
+import { database } from "..";
 
 
 
 // Default Deck
 const DEFAULT_DECK = {
-    id: -1,
-    title: 'None',
-    description: '',
+  id: -1,
+  title: 'None',
+  description: '',
 }
 
 const MAX_DESCRIPTION_LENGTH = 100;
 
-
-
 export function DeckDropDown({deck, cardArray, setCardArray}) {
 
-    const handleAdd = async () => { 
-        let { data, error } = await supabase
-            .rpc('fetch_test2', {
-                deckid: deck.id
-        })
+  const handleAdd = async () => { 
+    let { data, error } = await supabase
+      .rpc('fetch_test2', {
+        deckid: deck.id
+    })
 
-        if (error) {
-            console.log(error)
-        }
-        else {
-          // At this point, data shoudl be
-          console.log(data);
-        }
-
-        // TODO: Save to local DB.
-    
+    if (error) {
+      console.log(error)
+    }
+    else {
+      // At this point, data shoudl be
+      console.log(data);
     }
 
-    /* Possibly add a check if the deck is local database already?
-            - I think either: update or renders an unpressable 'Saved'
-    */
-    if (false) {
-      return (
-        <Button onPress={{}}>
-            Update
-        </Button>
-        
-        )
-    } else {
-      return (
-        <Button onPress={handleAdd}>
-          Save Deck
-        </Button>
-      )
+    // Save to local DB.
+    let arr = []
+    for (card of data) {
+      arr.push(await Card.makeIfAbsent(card))
     }
-
+    let dec = await Deck.update(deck)
+    await dec.setCards(arr)
   }
 
-export default function BrowseDecks() {
-    // Makes sure that it is only rendered once
-    const [init, setInit] = new useState(false);
-
-    // State variables, quite self-explanatory I think.
-    const [selectedDeck, setSelectedDeck] = new useState(DEFAULT_DECK);
-    const [deckData, setDeckData] = new useState([]);
-
-    // CardArray
-    const [cardArray, setCardArray] = new useState([]);
-
-
-
-    // Async function to fetch all decks from the database.
-    const fetchDecks = async () => {
-        const {data, error} = await supabase
-          .from('Decks')
-          .select('*')
-        if (error) {
-            setErrMsg(error.message);
-
-            // Log for testing purposes
-            console.log('error in obtaining decks')
-            console.log(error);
-        } 
-        // Log for testing purposes
-        console.log(data);
-
-        setDeckData(data);
-    }
-
-    // Function to handle deck select (for ChoiceRender)
-    const handleDeckSelect = (deck) => {
-        setSelectedDeck(deck);
-        setCardArray([]); // Clear the cardArray when a new deck is selected (It seems to work, albeit kinda jank).
-        console.log(cardArray);
-      };
-    
-    // If not initialized, fetches all decks.
-    if (!init) {
-        fetchDecks();
-        setInit(true);
-    }
-    
+  /* Possibly add a check if the deck is local database already?
+    - I think either: update or renders an unpressable 'Saved'
+  */
+  if (false) {
     return (
-        <SafeAreaView>
-            {/* This section renders the top container that informs you what you have selected*/}
-            <View style={styles.button2}>
-
-            {/* 1st Text is title, 2nd text is description, but checks if description is too long and truncates accordingly */}
-              <Text style={styles.title}>{"Selected Deck: " + selectedDeck.title}</Text>
-              
-              <Text style={styles.desc}>{selectedDeck.description.length > MAX_DESCRIPTION_LENGTH
-                ? selectedDeck.description.substring(0, MAX_DESCRIPTION_LENGTH) + '...'
-                : selectedDeck.description}
-              </Text>
-            {/* Dropdown option renders if you have selected a deck (i.e deckid not -1) */}
-              {selectedDeck.id !== -1 && (
-                <DeckDropDown deck = {selectedDeck} cardArray = {cardArray} setCardArray = {setCardArray}/>
-                )}
-            </View>
-
-            <View>
-                {/* This flatlist renders data fetched from handleDeckSelect*/}
-                <FlatList
-                    style = {{}}
-                    data = {deckData}
-                    renderItem={({ item }) => (
-                        <ChoiceRender item={item} setItem={handleDeckSelect} selectedItem={selectedDeck} />
-                      )}
-                    keyExtractor={(item) => item.title}               
-                />
-            </View>
-        </SafeAreaView>
+      <Button onPress={{}}>
+        Update
+      </Button>
+      
     )
+  } else {
+    return (
+      <Button onPress={handleAdd}>
+        Save Deck
+      </Button>
+    )
+  }
+
 }
 
+export default function BrowseDecks() {
+  // Makes sure that it is only rendered once
+  const [init, setInit] = new useState(false);
+
+  // State variables, quite self-explanatory I think.
+  const [selectedDeck, setSelectedDeck] = new useState(DEFAULT_DECK);
+  const [deckData, setDeckData] = new useState([]);
+
+  // CardArray
+  const [cardArray, setCardArray] = new useState([]);
+
+
+
+  // Async function to fetch all decks from the database.
+  const fetchDecks = async () => {
+    const {data, error} = await supabase
+      .from('Decks')
+      .select('*')
+    if (error) {
+      setErrMsg(error.message);
+
+      // Log for testing purposes
+      console.log('error in obtaining decks')
+      console.log(error);
+    } 
+    // Log for testing purposes
+    console.log(data);
+
+    setDeckData(data);
+  }
+
+  // Function to handle deck select (for ChoiceRender)
+  const handleDeckSelect = (deck) => {
+    setSelectedDeck(deck);
+    setCardArray([]); // Clear the cardArray when a new deck is selected (It seems to work, albeit kinda jank).
+    console.log(cardArray);
+  };
+  
+  // If not initialized, fetches all decks.
+  if (!init) {
+    fetchDecks();
+    setInit(true);
+  }
+  
+  return (
+    <SafeAreaView>
+      {/* This section renders the top container that informs you what you have selected*/}
+      <View style={styles.button2}>
+
+        {/* 1st Text is title, 2nd text is description, but checks if description is too long and truncates accordingly */}
+        <Text style={styles.title}>{"Selected Deck: " + selectedDeck.title}</Text>
+        
+        <Text style={styles.desc}>{selectedDeck.description.length > MAX_DESCRIPTION_LENGTH
+          ? selectedDeck.description.substring(0, MAX_DESCRIPTION_LENGTH) + '...'
+          : selectedDeck.description}
+        </Text>
+        {/* Dropdown option renders if you have selected a deck (i.e deckid not -1) */}
+        {selectedDeck.id !== -1 && (
+          <DeckDropDown deck = {selectedDeck} cardArray = {cardArray} setCardArray = {setCardArray}/>
+          )}
+      </View>
+
+      <View>
+        {/* This flatlist renders data fetched from handleDeckSelect*/}
+        <FlatList
+          style = {{}}
+          data = {deckData}
+          renderItem={({ item }) => (
+            <ChoiceRender item={item} setItem={handleDeckSelect} selectedItem={selectedDeck} />
+          )}
+          keyExtractor={(item) => item.title}               
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
     button: {
